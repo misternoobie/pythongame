1719db2dacc3ca426d526fe5644ed83050bebddf
Ron <misternoobie@users.noreply.github.com>
2021-04-05 14:32:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,20 +45,27 @@ class character:
     def getImage(self):
         return self.image
 
+class weapon:
+    def __init__(self, type):
+        self.type = type
+        if type == "bamboo":
+            image = pygame.image.load('bamboo.png')
+            self.image = pygame.transform.scale(image, (25, 25))
+            self.X = 0
+            self.Y = 0
+            self.movementX = 0.5
+            self.movementY = 2
+            self.state = "ready"
+            print("Bamboo Created: ", self.X, " | ", self.Y)
+    def getImage(self):
+        return self.image
+
 # initialize player and boss
 enemyList = []
 hero = character("hero")
 boss = character("boss")
+bamboo = weapon("bamboo")
 score_value = 0
-
-# knife
-knife = pygame.image.load('bamboo.png')
-knife = pygame.transform.scale(knife, (25,25))
-knifeX = 0
-knifeY = 0
-knife_moveX = 0.5
-knife_moveY = 2
-knife_state = "ready"
 
 gameover = False
 
@@ -105,20 +112,20 @@ def load_player(x,y):
 def load_boss():
     screen.blit(boss.getImage(), (boss.X,boss.Y))
 
-def fire(x, y):
-    global knife_state
-    knife_state = "fire"
-    screen.blit(knife, (x+25,y+10))
+def fire(x, y, weapon):
+    #global knife_state
+    weapon.state = "fire"
+    screen.blit(weapon.getImage(), (x+25,y+10))
 
-def collideAction(enemy):
-    global knife_state
+def collideAction(enemy,weapon):
+    #global knife_state
     global score_value
-    if knife_state != "ready":
+    if weapon.state != "ready":
         bullet_sound = mixer.Sound('game over.wav')
         bullet_sound.set_volume(0.1)
         bullet_sound.play()
         score_value += 1
-    knife_state = "ready"
+    weapon.state = "ready"
     enemy.X = 0
     enemy.y = 0
     enemy.movementX = ENEMY_MOVEMENT_SPEED
@@ -130,59 +137,48 @@ def stopEnemies(enemyList):
         enemy.X = 0
         enemy.y = 0
 
-
-generateEnemy(NUMBER_OF_ENEMIES)
-running = True
-# background
-background = pygame.image.load('country-platform-preview.png')
-background = pygame.transform.scale(background, (800, 600))
-screen.fill((22, 36, 27))
-while running:
-    screen.blit(background, (0, 0))
-    for enemy in enemyList:
-        screen.blit(enemy.getImage(), (enemy.X, enemy.Y))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+def keyInputChecker(event):
+    global running
+    global speedUp
+    global speedDown
+    global movementX
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            hero.movementX = -movementX
+            print("LEFT CLICKED")
+        if event.key == pygame.K_RIGHT:
+            hero.movementX = movementX
+            print("RIGHT CLICKED")
+        if event.key == pygame.K_DOWN:
+            hero.movementY = 1
+            print("LEFT CLICKED")
+        if event.key == pygame.K_UP:
+            hero.movementY = -1
+            print("RIGHT CLICKED")
+        if event.key == pygame.K_SPACE:
+            fire(bamboo.X, bamboo.Y, bamboo)
+            print("FIRE")
+        if event.key == pygame.K_1:
+            speedUp += 0.3
+            movementX += speedUp
+            print("Speed up: ", movementX)
+        if event.key == pygame.K_2:
+            if movementX > 0.1:
+                speedDown += 0.1
+                movementX -= speedDown
+            print("Speed down: ", movementX)
+        if event.key == pygame.K_ESCAPE:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                hero.movementX = -movementX
-                print("LEFT CLICKED")
-            if event.key == pygame.K_RIGHT:
-                hero.movementX = movementX
-                print("RIGHT CLICKED")
-            if event.key == pygame.K_DOWN:
-                hero.movementY = 1
-                print("LEFT CLICKED")
-            if event.key == pygame.K_UP:
-                hero.movementY = -1
-                print("RIGHT CLICKED")
-            if event.key == pygame.K_SPACE:
-                fire(knifeX, knifeY)
-                print("FIRE")
-            if event.key == pygame.K_1:
-                speedUp += 0.3
-                movementX += speedUp
-                print("Speed up: ", movementX)
-            if event.key == pygame.K_2:
-                if movementX > 0.1:
-                    speedDown += 0.1
-                    movementX -= speedDown
-                print("Speed down: ", movementX)
-            if event.key == pygame.K_ESCAPE:
-                running = False
-                break
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            hero.movementX = 0
+            print("Released")
+        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+            hero.movementY = 0
+            print("Released")
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                hero.movementX = 0
-                print("Released")
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                hero.movementY = 0
-                print("Released")
-
+def basicHeroMovements():
     if hero.X <= 0:
         hero.X = 0
     if hero.X >= 750:
@@ -191,47 +187,66 @@ while running:
         hero.Y = 550
     if hero.Y <= 0:
         hero.Y = 0
-
-    # weapon validation
-    if knife_state == "fire":
-        fire(knifeX, knifeY)
-        knifeY -= knife_moveY
-    if knifeY <= 0:
-        knife_state = "ready"
-    if knife_state == "ready":
-        knifeY = hero.Y
-        knifeX = hero.X
-
     hero.X += hero.movementX
     hero.Y += hero.movementY
 
+def basicWeaponMovement(weapon, hero):
+    # weapon validation
+    if weapon.state == "fire":
+        fire(weapon.X, weapon.Y, weapon)
+        weapon.Y -= weapon.movementY
+    if weapon.Y <= 0:
+        weapon.state = "ready"
+    if weapon.state == "ready":
+        weapon.Y = hero.Y
+        weapon.X = hero.X
+
+def basicEnemyMovement(enemy):
+    if enemy.X <= 0:
+        enemy.movementX = math.fabs(enemy.movementX)
+        enemy.Y += 20
+    if enemy.X >= 750:
+        enemy.movementX = -1 * enemy.movementX
+        enemy.Y += 20
+    enemy.X += enemy.movementX
+
+generateEnemy(NUMBER_OF_ENEMIES)
+running = True
+# background
+background = pygame.image.load('country-platform-preview.png')
+background = pygame.transform.scale(background, (800, 600))
+screen.fill((22, 36, 27))
+
+while running:
+    screen.blit(background, (0, 0))
+    basicHeroMovements()
+    basicWeaponMovement(bamboo, hero)
     scoreHUD()
     speedHUD()
     load_player(hero.X,hero.Y)
+    for enemy in enemyList:
+        screen.blit(enemy.getImage(), (enemy.X, enemy.Y))
+
+    for event in pygame.event.get():
+        keyInputChecker(event)
+        if event.type == pygame.QUIT:
+            running = False
 
     for enemy in enemyList:
-        if enemy.X <= 0:
-            enemy.movementX = math.fabs(enemy.movementX)
-            enemy.Y += 20
-        if enemy.X >= 750:
-            enemy.movementX = -1*enemy.movementX
-            enemy.Y += 20
+        basicEnemyMovement(enemy)
+        collision = isCollision(enemy.X, enemy.Y, bamboo.X, bamboo.Y)
+        playerCollision = isCollision(enemy.X, enemy.Y, hero.X, hero.Y)
+        if collision:
+            collideAction(enemy, bamboo)
+        if playerCollision:
+            hero.movementX = 0
+            hero.movementY = 0
+            stopEnemies(enemyList)
         if enemy.Y >= 300:
             print("Game Over")
             for boss in enemyList:
                 boss.X = 2000
             gameoverScreen()
             break
-        enemy.X += enemy.movementX
-
-        collision = isCollision(enemy.X, enemy.Y, knifeX, knifeY)
-        playerCollision = isCollision(enemy.X, enemy.Y, hero.X, hero.Y)
-
-        if collision:
-            collideAction(enemy)
-        if playerCollision:
-            hero.movementX = 0
-            hero.movementY = 0
-            stopEnemies(enemyList)
 
     pygame.display.update()
