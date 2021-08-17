4235f0999b2a8be64f083fe4a58990295139396d
Ron <misternoobie@users.noreply.github.com>
2021-08-17 21:44:42 +0800

    testing only
    test change

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from pygame import mixer
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 pygame.init()
 
 start_up = Initialization
